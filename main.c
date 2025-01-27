8e74aab2e2a8c85a7642538b519610a187c383ce
Aref Daei <aref.daei@outlook.com>
2025-01-27 23:32:59 +0330

    Initial commit

---
+++ main.c
@@ -0,0 +1,149 @@
+#include <stdio.h>
+#include <string.h>
+
+#define CBD 10 // Chess Board Dimensions
+
+void displayBoard(char board[][CBD]);
+void switchTurn(char board[][CBD]);
+void move();
+
+int isValidPawnMove();
+int isValidRookMove();
+int isValidKnightMove();
+int isValidBishopMove();
+int isValidQueenMove();
+int isValidKingMove();
+int isCheckMate();
+
+int main()
+{
+    char board[][CBD] = {
+        "-abcdefgh-",
+        "8rnbqkbnr8",
+        "7pppppppp7",
+        "6        6",
+        "5        5",
+        "4        4",
+        "3        3",
+        "2PPPPPPPP2",
+        "1RNBQKBNR1",
+        "-abcdefgh-",
+    };
+
+    int turn = 1;
+
+    while (1)
+    {
+        displayBoard(board);
+
+        char command[10];
+        printf("Command:   [command]\nExit:      exit    e\n> ");
+        scanf("%s", command);
+
+        char log[100];
+
+        switch (command[0])
+        {
+        case 'P':
+            isValidPawnMove();
+            break;
+
+        case 'R':
+            isValidRookMove();
+            break;
+
+        case 'N':
+            isValidKnightMove();
+            break;
+
+        case 'B':
+            isValidBishopMove();
+            break;
+
+        case 'Q':
+            isValidQueenMove();
+            break;
+
+        case 'K':
+            isValidKingMove();
+            break;
+
+        case 'e':
+            printf("Exiting the game...\n");
+            break;
+
+        default:
+            printf("The command is invalid!\n");
+            continue;
+        }
+
+        if (command[0] == 'e')
+            break;
+
+        if (isCheckMate())
+        {
+            printf("%s\n", log);
+            break;
+        }
+
+        switchTurn(board);
+        turn == 1 ? (turn = 2) : (turn = 1);
+    }
+
+    return 0;
+}
+
+void displayBoard(char board[][CBD])
+{
+    const char *colorWhite = "\x1b[1;34;47m"; // White
+    const char *colorBlack = "\x1b[1;34;40m"; // Black
+    const char *colorGray = "\x1b[3;90m";     // Gray
+    const char *colorReset = "\x1b[0m";       // Reset color
+
+    for (int i = 0; i < CBD; i++)
+    {
+        for (int j = 0; j < CBD; j++)
+        {
+            if (i % (CBD - 1) == 0 || j % (CBD - 1) == 0)
+                printf("%s%c %s", colorGray, board[i][j], colorReset);
+            else if ((i + j) % 2 == 0)
+                printf("%s%c %s", colorWhite, board[i][j], colorReset);
+            else
+                printf("%s%c %s", colorBlack, board[i][j], colorReset);
+        }
+        printf("\n");
+    }
+}
+
+void switchTurn(char board[][CBD])
+{
+    char temp[CBD][CBD];
+
+    for (int i = 0; i < CBD; i++)
+    {
+        for (int j = 0; j < CBD; j++)
+            temp[i][j] = board[CBD - 1 - i][CBD - 1 - j];
+    }
+
+    for (int i = 0; i < CBD; i++)
+    {
+        for (int j = 0; j < CBD; j++)
+            board[i][j] = temp[i][j];
+    }
+}
+
+void move() {}
+
+int isValidPawnMove() {}
+
+int isValidRookMove() {}
+
+int isValidKnightMove() {}
+
+int isValidBishopMove() {}
+
+int isValidQueenMove() {}
+
+int isValidKingMove() {}
+
+int isCheckMate() {}
