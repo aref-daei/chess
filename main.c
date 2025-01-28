7416f3448a9a87aa36fa717ddce2b376fd040a2c
Aref Daei <aref.daei@outlook.com>
2025-01-28 10:48:34 +0330

    Update main.c

--- main.c
+++ main.c
@@ -3,16 +3,16 @@
 
 #define CBD 10 // Chess Board Dimensions
 
-void displayBoard(char board[][CBD]);
-void switchTurn(char board[][CBD]);
-void move();
+void displayBoard(char board[][CBD], char log[]);
+void switchTurn(char board[][CBD], int *turn);
+void move(char board[][CBD], char command[], char log[]);
 
-int isValidPawnMove();
-int isValidRookMove();
-int isValidKnightMove();
-int isValidBishopMove();
-int isValidQueenMove();
-int isValidKingMove();
+int isValidPawnMove(char board[][CBD], char command[], int turn, char log[]);
+int isValidRookMove(char board[][CBD], char command[], int turn, char log[]);
+int isValidKnightMove(char board[][CBD], char command[], int turn, char log[]);
+int isValidBishopMove(char board[][CBD], char command[], int turn, char log[]);
+int isValidQueenMove(char board[][CBD], char command[], int turn, char log[]);
+int isValidKingMove(char board[][CBD], char command[], int turn, char log[]);
 int isCheckMate();
 
 int main()
@@ -34,43 +34,43 @@ int main()
 
     while (1)
     {
-        displayBoard(board);
+        char log[128];
 
-        char command[10];
+        displayBoard(board, log);
+
+        char command[8];
         printf("Command:   [command]\nExit:      exit\n> ");
         scanf("%s", command);
 
-        char log[100];
-
         if (command[0] == 'P')
         {
-            isValidPawnMove();
+            isValidPawnMove(board, command, turn, log);
         }
         else if (command[0] == 'R')
         {
-            isValidRookMove();
+            isValidRookMove(board, command, turn, log);
         }
         else if (command[0] == 'N')
         {
-            isValidKnightMove();
+            isValidKnightMove(board, command, turn, log);
         }
         else if (command[0] == 'B')
         {
-            isValidBishopMove();
+            isValidBishopMove(board, command, turn, log);
         }
         else if (command[0] == 'Q')
         {
-            isValidQueenMove();
+            isValidQueenMove(board, command, turn, log);
         }
         else if (command[0] == 'K')
         {
-            isValidKingMove();
+            isValidKingMove(board, command, turn, log);
         }
         else if (!strcmp(command, "exit"))
             break;
         else
         {
-            printf("The command is invalid!\n");
+            strcpy(log, "The command is invalid!");
             continue;
         }
 
@@ -80,14 +80,13 @@ int main()
             break;
         }
 
-        switchTurn(board);
-        turn == 1 ? (turn = 2) : (turn = 1);
+        switchTurn(board, &turn);
     }
 
     return 0;
 }
 
-void displayBoard(char board[][CBD])
+void displayBoard(char board[][CBD], char log[])
 {
     const char *colorWhite = "\x1b[1;34;47m"; // White
     const char *colorBlack = "\x1b[1;34;40m"; // Black
@@ -105,11 +104,15 @@ void displayBoard(char board[][CBD])
             else
                 printf("%s%c %s", colorBlack, board[i][j], colorReset);
         }
+
+        if (i == 0 && log[0] != '\0')
+            printf("    Log: %s", log);
+
         printf("\n");
     }
 }
 
-void switchTurn(char board[][CBD])
+void switchTurn(char board[][CBD], int *turn)
 {
     char temp[CBD][CBD];
 
@@ -124,20 +127,184 @@ void switchTurn(char board[][CBD])
         for (int j = 0; j < CBD; j++)
             board[i][j] = temp[i][j];
     }
+
+    *turn == 1 ? (*turn = 2) : (*turn = 1);
 }
 
-void move() {}
+void move(char board[][CBD], char command[], char log[]) {}
 
-int isValidPawnMove() {}
+int isValidPawnMove(char board[][CBD], char command[], int turn, char log[])
+{
+    switch (turn)
+    {
+    case 1:
+        if (board[CBD - (command[2] - 49 + 2)][command[1] - 97 + 1] != 'P')
+        {
+            strcpy(log, "The piece you chose is wrong.");
+            return 0;
+        }
+        printf("POOOOOOO\n");
+        break;
 
-int isValidRookMove() {}
+    case 2:
+        if (board[command[2] - 49 + 1][CBD - (command[1] - 97 + 1)] != 'p')
+        {
+            strcpy(log, "The piece you chose is wrong.");
+            return 0;
+        }
+        printf("pOOOOOOO\n");
+        break;
 
-int isValidKnightMove() {}
+    default:
+        break;
+    }
 
-int isValidBishopMove() {}
+    return 1;
+}
 
-int isValidQueenMove() {}
+int isValidRookMove(char board[][CBD], char command[], int turn, char log[])
+{
+    switch (turn)
+    {
+    case 1:
+        if (board[CBD - (command[2] - 49 + 2)][command[1] - 97 + 1] != 'R')
+        {
+            strcpy(log, "The piece you chose is wrong.");
+            return 0;
+        }
+        printf("ROOOOOOO\n");
+        break;
 
-int isValidKingMove() {}
+    case 2:
+        if (board[command[2] - 49 + 1][CBD - (command[1] - 97 + 1)] != 'r')
+        {
+            strcpy(log, "The piece you chose is wrong.");
+            return 0;
+        }
+        printf("rOOOOOOO\n");
+        break;
+
+    default:
+        break;
+    }
+
+    return 1;
+}
+
+int isValidKnightMove(char board[][CBD], char command[], int turn, char log[])
+{
+    switch (turn)
+    {
+    case 1:
+        if (board[CBD - (command[2] - 49 + 2)][command[1] - 97 + 1] != 'N')
+        {
+            strcpy(log, "The piece you chose is wrong.");
+            return 0;
+        }
+        printf("NOOOOOOO\n");
+        break;
+
+    case 2:
+        if (board[command[2] - 49 + 1][CBD - (command[1] - 97 + 1)] != 'n')
+        {
+            strcpy(log, "The piece you chose is wrong.");
+            return 0;
+        }
+        printf("nOOOOOOO\n");
+        break;
+
+    default:
+        break;
+    }
+
+    return 1;
+}
+
+int isValidBishopMove(char board[][CBD], char command[], int turn, char log[])
+{
+    switch (turn)
+    {
+    case 1:
+        if (board[CBD - (command[2] - 49 + 2)][command[1] - 97 + 1] != 'B')
+        {
+            strcpy(log, "The piece you chose is wrong.");
+            return 0;
+        }
+        printf("BOOOOOOO\n");
+        break;
+
+    case 2:
+        if (board[command[2] - 49 + 1][CBD - (command[1] - 97 + 1)] != 'b')
+        {
+            strcpy(log, "The piece you chose is wrong.");
+            return 0;
+        }
+        printf("bOOOOOOO\n");
+        break;
+
+    default:
+        break;
+    }
+
+    return 1;
+}
+
+int isValidQueenMove(char board[][CBD], char command[], int turn, char log[])
+{
+    switch (turn)
+    {
+    case 1:
+        if (board[CBD - (command[2] - 49 + 2)][command[1] - 97 + 1] != 'Q')
+        {
+            strcpy(log, "The piece you chose is wrong.");
+            return 0;
+        }
+        printf("QOOOOOOO\n");
+        break;
+
+    case 2:
+        if (board[command[2] - 49 + 1][CBD - (command[1] - 97 + 1)] != 'q')
+        {
+            strcpy(log, "The piece you chose is wrong.");
+            return 0;
+        }
+        printf("qOOOOOOO\n");
+        break;
+
+    default:
+        break;
+    }
+
+    return 1;
+}
+
+int isValidKingMove(char board[][CBD], char command[], int turn, char log[])
+{
+    switch (turn)
+    {
+    case 1:
+        if (board[CBD - (command[2] - 49 + 2)][command[1] - 97 + 1] != 'K')
+        {
+            strcpy(log, "The piece you chose is wrong.");
+            return 0;
+        }
+        printf("KOOOOOOO\n");
+        break;
+
+    case 2:
+        if (board[command[2] - 49 + 1][CBD - (command[1] - 97 + 1)] != 'k')
+        {
+            strcpy(log, "The piece you chose is wrong.");
+            return 0;
+        }
+        printf("kOOOOOOO\n");
+        break;
+
+    default:
+        break;
+    }
+
+    return 1;
+}
 
 int isCheckMate() {}
